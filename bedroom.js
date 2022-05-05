6e1ff2206e23ccdca2d14eab8bd525204eb85d73
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-05-05 19:49:46 +1000

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -1,4 +1,5 @@
 statius="";
+objects=[];
 function preload(){
     img=loadImage("bedroom.jpg");
 }
@@ -18,4 +19,18 @@ function gotResult(error,results){
         console.error(error);
      }
      console.log(results);
+     objects=results;
+}
+function draw(){
+    image(img,0,0,640,420);
+    if(statius != ""){
+        for(i=0; i<objects.length; i++){
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
